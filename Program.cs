fcfcbbe0891bead740713f571999a462c06aa886
Joke Heyndels <jo_hannah@outlook.com>
2020-09-16 10:48:32 +0200

    Add char arrays to hold the secret message and the encrypted message

--- Program.cs
+++ Program.cs
@@ -8,6 +8,11 @@ namespace CaesarCipher
         {
             char[] alphabet = new char[] {'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'};
 
+            Console.WriteLine("Type your message to encrypt:");
+
+            char[] secretMessage = Console.ReadLine().ToCharArray();
+
+            char[] encryptedMessage = new char[secretMessage.Length]; 
         }
     }
 }
