dca5fd3d4eb4e3f0ba66115f501f813773646c4d
Joke Heyndels <jo_hannah@outlook.com>
2020-09-16 20:31:14 +0200

    Create encryption for loop. End of guided project.

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Linq;
+using System.Transactions;
 
 namespace CaesarCipher
 {
@@ -10,9 +12,31 @@ namespace CaesarCipher
 
             Console.WriteLine("Type your message to encrypt:");
 
-            char[] secretMessage = Console.ReadLine().ToCharArray();
+            char[] secretMessage = Console.ReadLine().ToLower().ToCharArray();
 
             char[] encryptedMessage = new char[secretMessage.Length]; 
+
+            for (int i = 0; i < secretMessage.Length; i++) 
+            {
+                char currentChar = secretMessage[i];
+                char encryptedChar;
+                if (alphabet.Contains(currentChar)) 
+                {
+                    int currentCharAlphaIndex = Array.IndexOf(alphabet, currentChar);
+                    int newAlphaIndex = currentCharAlphaIndex + 3;
+                    int alphaIndex = newAlphaIndex < alphabet.Length ? newAlphaIndex : newAlphaIndex - alphabet.Length;
+                    encryptedChar = alphabet[alphaIndex];
+                }
+                else
+                {
+                    encryptedChar = currentChar;
+                }
+                
+                encryptedMessage[i] = encryptedChar;
+            }
+
+            string encryptedMessageString = String.Join("", encryptedMessage);
+            Console.WriteLine(encryptedMessageString);
         }
     }
 }
